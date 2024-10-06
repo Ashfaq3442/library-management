c155f298a6e9a360c9fa05c925db292e1590877a
Ashfaq3442 <ashfaqm46@gmail.com>
2024-10-06 18:06:35 +0500

    add new fields in model.py for book availability and for the person who borrowed the book

--- books/models.py
+++ books/models.py
@@ -4,6 +4,10 @@ class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=200)
     description=models.TextField()
+    is_available = models.BooleanField(default=True)  # Field for availability
+    borrower_name = models.CharField(max_length=100, blank=True, null=True)
+    borrower_contact = models.CharField(max_length=100, blank=True, null=True)
+    borrower_address = models.TextField(blank=True, null=True)
 
 
     def __str__(self):
