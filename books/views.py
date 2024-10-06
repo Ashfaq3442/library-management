690945e10042ba2c7a31c555869a2ef0188d47cb
Ashfaq3442 <ashfaqm46@gmail.com>
2024-10-06 15:33:19 +0500

    create views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
+
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
 
 # Create your views here.
