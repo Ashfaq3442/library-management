2028bc6d618ac321b926603427fbca03b76ecfa7
Ashfaq3442 <ashfaqm46@gmail.com>
2024-10-07 00:18:15 +0500

    add a new url in urls.py in the app for the return book functionality

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('add/', views.add_book, name='add_book'),
     path('', views.book_list, name='book_list'),
+    path('return/<int:book_id>/', views.return_book, name='return_book'),
 ]
